8b46906511978c5da56efe148356720fa94734af
SlimTim10 <slimtim10@gmail.com>
2022-12-10 10:25:06 -0500

    Finish Day 4 part 1

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Day1.Main as Day1
 import qualified Day2.Main as Day2
 import qualified Day3.Main as Day3
+import qualified Day4.Main as Day4
 
 main :: IO ()
 main = do
@@ -20,3 +21,8 @@ main = do
   Day3.test
   Day3.part1
   Day3.part2
+
+  putStrLn "Day4"
+  Day4.test
+  Day4.part1
+  -- Day4.part2
